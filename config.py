ee25515323647fd78b9b46ba897ac300a9d6d6df
zhhehao <zh.hehao@gmail.com>
2016-10-10 22:06:41 +0800

    complete partIII

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '\x13*\xbb\xe6\xdf\x10!S\xf9\xf6Q\xf5\\\xdb$<j\x8c\xb6\xb3\x08\x12\xe4\x9c'
+
+OPENID_PROVIDERS = [
+	{'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+	{'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+	{'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+	{'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
