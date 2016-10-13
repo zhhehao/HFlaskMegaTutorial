5918e71d31f7d0f08a176f5bd4ebd20db47b066d
zhhehao <zh.hehao@gmail.com>
2016-10-13 16:12:26 +0800

    complete part 4

--- db_create.py
+++ db_create.py
@@ -1,13 +1,13 @@
 #!flask/bin/python
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
-from config import SQLALCHEYM_MIGRATE_REPO
+from config import SQLALCHEMY_MIGRATE_REPO
 from app import db
 import os.path
 db.create_all()
-if not os.path.exists(SQLALCHEYM_MIGRATE_REPO):
-	api.create(SQLALCHEYM_MIGRATE_REPO, 'database repository')
-	api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEYM_MIGRATE_REPO)
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+	api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+	api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 else:
-	avi.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEYM_MIGRATE_REPO, avi.version(SQLALCHEYM_MIGRATE_REPO))
+	avi.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, avi.version(SQLALCHEYM_MIGRATE_REPO))
 
